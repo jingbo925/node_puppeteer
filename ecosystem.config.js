92bb111366a0d0271c3becafa39f836f4b800d08
wangjingbo <wangjingbo@cella.xyz>
2018-05-29 14:12:09 +0800

    pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'orange',
+      script    : 'start.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'duolong',
+      host : '47.94.152.70',
+      ref  : 'origin/master',
+      repo: 'git@oranger:jingbo925/orange.git',
+      path : '/srv/www',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+  }
+};
